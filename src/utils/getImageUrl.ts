420095e78dfa2e2ac7764efe9c6897ab8870eceb
pdemianowicz <pdemianowicz@gmail.com>
2025-10-12 15:42:35 +0200

    fix(getImageUrl): Correctly type path parameter to accept null

--- src/utils/getImageUrl.ts
+++ src/utils/getImageUrl.ts
@@ -1,5 +1,5 @@
 const BASE_IMG_URL = "https://image.tmdb.org/t/p";
 
-export default function getImageUrl(path: string, size: string = "w500") {
+export default function getImageUrl(path: string | null, size: string = "w500") {
   return `${BASE_IMG_URL}/${size}${path}`;
 }
