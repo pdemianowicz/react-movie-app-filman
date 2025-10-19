6539c37d062a4b5b3f73127b9b71159198e0b232
pdemianowicz <pdemianowicz@gmail.com>
2025-10-19 12:26:28 +0200

    refactor(hooks): adopt useQuery for useMovieDetails
    Replaces the manual fetch logic in `useMovieDetails` with a conditional `useQuery` hook. This simplifies the code and makes data fetching dependent on the `id` param being present.

--- src/hooks/useMediaDetails.tsx
+++ src/hooks/useMediaDetails.tsx
@@ -1,44 +1,20 @@
-import { useEffect, useState } from "react";
-import type { MediaDetails, MovieDetails, PersonDetails, TvDetails } from "../types/tmdb.types";
 import { useParams } from "react-router-dom";
 import { getMovieDetails, getPersonDetails, getTvDetails } from "../utils/tmdbApi";
+import { useQuery } from "@tanstack/react-query";
 
-type MediaDetailsMap = {
-  movie: MovieDetails;
-  tv: TvDetails;
-  person: PersonDetails;
-};
-
-export default function useMovieDetails<T extends keyof MediaDetailsMap>(mediaType: T) {
-  const [data, setData] = useState<MediaDetailsMap[T] | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+export default function useMovieDetails(mediaType: "movie" | "tv" | "person") {
   const { id } = useParams();
 
-  useEffect(() => {
-    if (!id) return;
+  return useQuery({
+    queryKey: ["details", mediaType, id],
+    queryFn: async () => {
+      if (!id) return;
 
-    setData(null);
-    setLoading(true);
-    setError("");
+      if (mediaType === "movie") return getMovieDetails(Number(id));
+      if (mediaType === "tv") return getTvDetails(Number(id));
+      if (mediaType === "person") return getPersonDetails(Number(id));
+    },
 
-    async function fetchData() {
-      try {
-        let result: MediaDetails | null = null;
-        if (mediaType === "movie") result = await getMovieDetails(Number(id));
-        if (mediaType === "tv") result = await getTvDetails(Number(id));
-        if (mediaType === "person") result = await getPersonDetails(Number(id));
-        setData(result as MediaDetailsMap[T]);
-      } catch (err) {
-        console.error("Error fetching details:", err);
-        setError("Failed to load details. Please try again.");
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchData();
-  }, [mediaType, id]);
-
-  return { data, loading, error };
+    enabled: !!id,
+  });
 }
