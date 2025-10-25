c52443a2efc4da25a032df17a780b68baf97819f
pdemianowicz <pdemianowicz@gmail.com>
2025-10-25 19:11:55 +0200

    feat(ui): add page transitions using Framer Motion
    Integrates `AnimatePresence` into the root `App` component to create a fade-and-slide effect when navigating between routes, improving the overall look and feel of the application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/nav/Header";
 import Home from "./pages/Home";
 import Bookmarks from "./pages/Bookmarks";
@@ -6,22 +6,34 @@ import PersonDetails from "./pages/PersonDetails";
 import Footer from "./components/Footer";
 import DiscoverPage from "./pages/DiscoverPage";
 import MediaDetailsPage from "./pages/MediaDetailsPage";
+import { AnimatePresence, motion } from "framer-motion";
 
 function App() {
+  const location = useLocation();
+
   return (
     <main className="flex flex-col min-h-screen bg-background text-text-primary">
       <Header />
-      <div className="flex-grow p-4 md:p-6 overflow-auto max-w-7xl mx-auto mt-8 w-full">
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/movies" element={<DiscoverPage mediaType="movie" />} />
-          <Route path="/serials" element={<DiscoverPage mediaType="tv" />} />
-          <Route path="/bookmarks" element={<Bookmarks />} />
+      <div className="flex-grow p-4 md:p-6 overflow-hidden max-w-7xl mx-auto mt-8 w-full">
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={location.pathname}
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 50 }}
+            transition={{ duration: 0.3 }}>
+            <Routes location={location}>
+              <Route index element={<Home />} />
+              <Route path="/movies" element={<DiscoverPage mediaType="movie" />} />
+              <Route path="/serials" element={<DiscoverPage mediaType="tv" />} />
+              <Route path="/bookmarks" element={<Bookmarks />} />
 
-          <Route path="/movie/:id" element={<MediaDetailsPage mediaType="movie" />} />
-          <Route path="/tv/:id" element={<MediaDetailsPage mediaType="tv" />} />
-          <Route path="/person/:id" element={<PersonDetails />} />
-        </Routes>
+              <Route path="/movie/:id" element={<MediaDetailsPage mediaType="movie" />} />
+              <Route path="/tv/:id" element={<MediaDetailsPage mediaType="tv" />} />
+              <Route path="/person/:id" element={<PersonDetails />} />
+            </Routes>
+          </motion.div>
+        </AnimatePresence>
       </div>
       <Footer />
     </main>
