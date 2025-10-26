7d94b91233781918254c083bd169066ba96212d1
pdemianowicz <pdemianowicz@gmail.com>
2025-10-26 07:33:28 +0100

    feat: add ScrollToTop component to reset scroll position on route change

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ScrollToTop from "./components/ScrollToTop.tsx";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <HashRouter>
+        <ScrollToTop />
         <App />
       </HashRouter>
     </QueryClientProvider>
